62832083b26d5efa395c7e27f52579007839d3d1
TingSong-Syu <rek@rek.me>
2021-01-16 19:53:20 +0800

    Add a message to Notification helper class

---
+++ src/GroupNotification.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NotificationChannels\WxWorkGroupBot;
+
+use Illuminate\Notifications\Notification;
+
+final class GroupNotification extends Notification
+{
+    private $message;
+
+    private function __construct() { }
+
+    public static function make(WxWorkGroupBotMessage $message): GroupNotification
+    {
+        $notification = new self();
+        $notification->message = $message;
+        return $notification;
+    }
+
+    public function toWxWorkGroupBot()
+    {
+        return $this->message;
+    }
+}
